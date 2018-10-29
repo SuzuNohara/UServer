833e18fa05f309abb4a71edbd7f7dd5f49b0531d
Suzu Nohara <suzu.nkdca@gmail.com>
2018-10-29 11:03:25 -0600

    Ajuste git ignore

--- logger.js
+++ logger.js
@@ -4,32 +4,39 @@ let fs = require('fs');
 function error(message){
   let date = new Date();
   console.log("ERROR: " + date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + " - " + date.getHours() + ":" + date.getMinutes() + "> " + message);
+  if(config.screenlogs){console.log(line);}
+  if(config.logs){writeLog(line);}
 }
 
 function info(message){
   let date = new Date();
   console.log("INFO: " + date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + " - " + date.getHours() + ":" + date.getMinutes() + "> " + message);
+  if(config.screenlogs){console.log(line);}
+  if(config.logs){writeLog(line);}
 }
 
 function warning(message){
   let date = new Date();
-  console.log("WARNING: " + date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + " - " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "> " + message);
+  let line = "WARNING: " + date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + " - " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "> " + message;
+  if(config.screenlogs){console.log(line);}
+  if(config.logs){writeLog(line);}
 }
 
-function checkDay(){
+function writeLog(line){
   let date = new Date();
   if(config.logs){
     fs.readFile(config.logsroute + 'LOG_' + date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear() + '.log', (error,datos) => {
-      if (error)
-        console.log(error);
-      else
-        console.log(datos.toString());
-    });
-    fs.writeFile('./archivo1.txt', 'línea 1\nLínea 2', error => {
-      if (error)
-        console.log(error);
-      else
-        console.log('El archivo fue creado');
+      if (error){
+        fs.writeFile('./archivo1.txt', 'línea 1\nLínea 2', error => {
+          if (error){
+
+          }else{
+
+          }
+        });
+      }else{
+
+      }
     });
   }
 }
